b1b55bb40c71af90c50080f7aea2bbde4a274e90
BGV_UFA <bgv-ufa@mail.ru>
2024-12-12 16:07:57 +0500

    12.12.24

---
+++ apanel/scripts/scripts.js
@@ -0,0 +1,8 @@
+
+const navmenu_burger = document.querySelector('.navmenu_burger');
+const mainMenu = document.querySelector('.mainMenu');
+
+navmenu_burger.addEventListener('click', () => {
+    navmenu_burger.classList.toggle('burger_active');
+    mainMenu.classList.toggle('burger_active');
+})
